da963de933c784de2586d6ca13d9ae5c1b459232
teymour-aldridge <teymour-aldridge@users.noreply.github.com>
2021-07-07 20:37:04 +0000

    deploy: ca347a0de65d2e99c7d123d7d45854dd0ae39a3f

--- index-1521ea88d50851d8.js
+++ index-1521ea88d50851d8.js
@@ -268,7 +268,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-2ffb4e4df3d4841b_bg.wasm', import.meta.url);
+        input = new URL('index-1521ea88d50851d8_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
